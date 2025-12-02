f2088c2af1484039907aa355b4c1a2f999189228
chxe01 <a01082311887l@gmail.com>
2025-12-02 11:30:37 +0900

    2562

---
+++ 2562.py
@@ -0,0 +1,18 @@
+N = []
+
+for i in range (9) :
+    a = int(input())
+    N.append(a)
+a = max(N)
+
+print(a)
+print(N.index(a) + 1)
+N = []
+
+for i in range (9) :
+    a = int(input())
+    N.append(a)
+a = max(N)
+
+print(a)
+print(N.index(a) + 1)
